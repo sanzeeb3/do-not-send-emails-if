72fff8cc8879f1fee884a341dbc4194eb8bd0b48
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2021-10-18 09:40:09 +0545

    Settings page initiate

--- assets/admin-settings.js
+++ assets/admin-settings.js
@@ -12,7 +12,41 @@ import { useState } from '@wordpress/element';
 
 export default function Settings() {
     return (
-        <h1> { __( 'Do Not Send Emails If', 'do-not-send-emails-if' ) } </h1>
+        <div id="do-not-send-emails-if-settings">
+            <h2> { __( 'Do Not Send Emails If', 'do-not-send-emails-if' ) } </h2>
+            
+            <div className="do-not-send-emails-if-conditional-settings">
+                <div className="do-not-send-emails-if-condition">
+                    <select name="">
+                        <option>To Email</option>
+                        <option>From Email</option>
+                        <option>Email Subject</option>
+                        <option>Email Content</option>
+                    </select>
+                </div>
+                
+                <div className="do-not-send-emails-if-matches">
+                    <select name="">
+                        <option>is</option>
+                        <option>is not</option>
+                        <option>emtpy</option>
+                        <option>not emtpy</option>
+                        <option>contains</option>
+                        <option>does not contain</option>
+                    </select>
+                </div>
+                
+                <div className="do-not-send-emails-if-result">
+                    <input type="text"/>
+                </div>
+
+            </div>
+
+            <p className="submit">
+                <button type="submit" className="button-primary">Save</button>
+            </p>
+
+        </div>
     )
 }
 
