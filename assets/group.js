200e4db5564894c4b14e8a305095e6df6c471841
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2021-10-21 10:17:36 +0545

    Few things learned, nothing mre

--- assets/group.js
+++ assets/group.js
@@ -5,89 +5,92 @@ export default function Group() {
 
 	const addNewRow = (e) => {
 		e.preventDefault();
-		setAddNewRowCount( addNewRowCount + 1 );
+
+ 		var dataId = parseInt( e.target.getAttribute('data-id') );
+
+ 		dataId = dataId + 1;
+
+ 		// addNewRowCount = [ ...addNewRowCount, dataId ];
+
+ 		setAddNewRowCount( [ ...addNewRowCount, dataId ] );
 	}
 
 	const deleteRow = (e) => {
 		e.preventDefault();
 
-		if ( addNewRowCount > 1 ) {
-			setAddNewRowCount( addNewRowCount - 1 );
-		}
+		var dataId = e.target.getAttribute('data-id');
 	}
 
-	const addNewGroup  = (e) => {
-		e.preventDefault();
+	const [addNewRowCount, setAddNewRowCount] = useState( [] );
 
-		setAddNewOrCount( addNewOrCount + 1 );
+	function andBlock( dataId = 0 ) {
+		return (
+			<div className="do-not-send-emails-if-conditional-group">
+
+				<div className="do-not-send-emails-if-condition">
+					<select name="do-not-send-emails-if-condition[]">
+						<option> { __( 'To Email', 'do-not-send-emails-if' ) } </option>
+						<option> { __( 'From Email', 'do-not-send-emails-if' ) } </option>
+						<option> { __( 'Email Subject', 'do-not-send-emails-if' ) } </option>
+						<option> { __( 'Email Message', 'do-not-send-emails-if' ) } </option>
+					</select>
+				</div>
+
+				<div className="do-not-send-emails-if-matches">
+					<select name="do-not-send-emails-if-matches[]">
+						<option> { __( 'is', 'do-not-send-emails-if' ) } </option>
+						<option> { __( 'is not', 'do-not-send-emails-if' ) } </option>
+						<option> { __( 'contains', 'do-not-send-emails-if' ) } </option>
+						<option> { __( 'does not contain', 'do-not-send-emails-if' ) } </option>
+					</select>
+				</div>
+
+				<div className="do-not-send-emails-if-result">
+					<input type="text" name="do-not-send-emails-if-result[]"/>
+				</div>
+
+				<div className="do-not-send-emails-if-plus">
+					<button data-id={dataId} onClick={addNewRow} className="button-primary">{ __( 'AND', 'do-not-send-emails-if' ) }</button>
+				</div>
+
+				<div className="do-not-send-emails-if-minus">
+					<span data-id={dataId} onClick={deleteRow} class="dashicons dashicons-trash"></span>
+				</div>
+
+			</div>
+		);
 	}
 
-	const [addNewRowCount, setAddNewRowCount] = useState( 1 );
-	const [addNewOrCount, setAddNewOrCount] = useState( 1 );
+	// var orBlock = (
+	// 	<div className="do-not-send-emails-if-or-group">
+	// 		<div className="do-not-send-emails-if-conditional-groups">
+	// 			{andBlock()}
+	// 		</div>
 
-	var andBlock = (
-		<div className="do-not-send-emails-if-conditional-group">
+	// 		<p> { __( 'or', 'do-not-send-emails-if') }, </p>
+	// 	</div>
+	// );
+	//
+	console.log( addNewRowCount );
 
-			<div className="do-not-send-emails-if-condition">
-				<select name="do-not-send-emails-if-condition[]">
-					<option> { __( 'To Email', 'do-not-send-emails-if' ) } </option>
-					<option> { __( 'From Email', 'do-not-send-emails-if' ) } </option>
-					<option> { __( 'Email Subject', 'do-not-send-emails-if' ) } </option>
-					<option> { __( 'Email Message', 'do-not-send-emails-if' ) } </option>
-				</select>
-			</div>
+	addNewRowCount.sort();
 
-			<div className="do-not-send-emails-if-matches">
-				<select name="do-not-send-emails-if-matches[]">
-					<option> { __( 'is', 'do-not-send-emails-if' ) } </option>
-					<option> { __( 'is not', 'do-not-send-emails-if' ) } </option>
-					<option> { __( 'contains', 'do-not-send-emails-if' ) } </option>
-					<option> { __( 'does not contain', 'do-not-send-emails-if' ) } </option>
-				</select>
-			</div>
+	// addNewRowCount.forEach( element => {
+	// 	var andBlockGroup = [ ...andBlockGroup, andBlock( element ) ]
+	// });
 
-			<div className="do-not-send-emails-if-result">
-				<input type="text" name="do-not-send-emails-if-result[]"/>
-			</div>
-
-			<div className="do-not-send-emails-if-plus">
-				<button onClick={addNewRow} className="button-primary">{ __( 'AND', 'do-not-send-emails-if' ) }</button>
-			</div>
-
-			<div className="do-not-send-emails-if-minus">
-				<span onClick={deleteRow} class="dashicons dashicons-trash"></span>
-			</div>
-
-		</div>
-	);
-
-	var orBlock = (
-		<div className="do-not-send-emails-if-or-group">
-			<div className="do-not-send-emails-if-conditional-groups">
-				{andBlock}
-			</div>
-
-			<p> { __( 'or', 'do-not-send-emails-if') }, </p>
-		</div>
-	);
-
-	for ( let i=0; i<addNewRowCount; i++ ) {
-		var	andBlockGroup = [ ...andBlockGroup, andBlock ];
+	for ( const val of addNewRowCount ) {
+		var andBlockGroup = [ ...andBlockGroup, andBlock( val ) ]
 	}
 
-	for ( let i=0; i<addNewOrCount; i++ ) {
-		var	orBlockGroup = [ ...orBlockGroup, orBlock ];
-	}
-
-	var altogether = [andBlockGroup, orBlockGroup];
 
 	return (
 		<div className="do-not-send-emails-if-conditional-settings">
 
-			{altogether}
+			{ andBlockGroup ? andBlockGroup : andBlock(0) }
 
 			<div className="do-not-send-emails-if-add-new-group">
-				<button onClick={addNewGroup} className="button-secondary">{__('Add New Group', 'do-not-send-emails-if')}</button>
+				<button className="button-secondary">{__('Add New Group', 'do-not-send-emails-if')}</button>
 			</div>
 
 		</div>
